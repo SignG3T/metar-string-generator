2fee9f8f7876852907f1ca71edef4ca7dd99223f
Niccolo4646 <nicolodaniello50@gmail.com>
2024-12-19 15:17:45 +0100

    Added History logic with LocalStorage

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+function addToHistory(metar) {
+  const timestamp = new Date().toISOString();
+  let history = JSON.parse(localStorage.getItem("history")) || [];
+  history.push({ metar, timestamp });
+  localStorage.setItem("history", JSON.stringify(history));
+}
+
+function showHistory() {
+  let history = JSON.parse(localStorage.getItem("history")) || [];
+  const outputHistory = document.getElementById("output-history");
+
+  if (history.length === 0) {
+    outputHistory.innerHTML = "<li>No history available</li>";
+  } else {
+    outputHistory.innerHTML = "";
+    history.forEach((item) => {
+      const listItem = document.createElement("li");
+      xx = JSON.stringify(item.metar);
+      x = xx.split("\"")
+      listItem.textContent = x[1];
+      outputHistory.appendChild(listItem);
+    });
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  showHistory();
+});
+
 document.getElementById('generate-btn').addEventListener('click', () => {
   const icao = document.getElementById('icao').value;
   const day = document.getElementById('day').value;
@@ -16,10 +45,13 @@ document.getElementById('generate-btn').addEventListener('click', () => {
   const metar = `METAR ${icao} ${day}${time}Z ${windSpeed}${windDirection} ${visibility} ${clouds} ${temperature}/${dewpoint} ${weather} Q${pressure} ${nosig}`
 
   document.getElementById('metar-output').textContent = metar;
+
+  addToHistory(metar)
 })
 
 document.getElementById('copy-btn').addEventListener('click', () => {
   const output = document.getElementById('metar-output').textContent;
+
   if(output === "-- Generated METAR will appear here --") {
     alert("Please generate a METAR first");
   } else {
